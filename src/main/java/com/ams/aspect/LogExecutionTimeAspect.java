de3097f0b9a23216bab785d4a3e0b40ce91279f4
sreenivasulureddy06 <sreenivasulureddy06@gmail.com>
2025-06-28 21:00:03 +0530

    Added loggers

--- src/main/java/com/ams/aspect/LogExecutionTimeAspect.java
+++ src/main/java/com/ams/aspect/LogExecutionTimeAspect.java
@@ -1,23 +1,24 @@
 package com.ams.aspect;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.slf4j.Logger;
 import org.springframework.stereotype.Component;
-
-import java.util.logging.LogManager;
 
 @Aspect
 @Component
 public class LogExecutionTimeAspect {
+
+    Logger logger = LogManager.getLogger(LogExecutionTimeAspect.class);
 
     @Around("@annotation(LogExecutionTime)")
     public Object invoke(ProceedingJoinPoint joinPoint) throws Throwable {
         long start = System.currentTimeMillis();
         var result = joinPoint.proceed();
         long end = System.currentTimeMillis();
-        System.out.println("Execution time of method("+joinPoint.getSignature().getName()+") is: "+(end - start)+" millis");
+        logger.info("Execution time of for Class[{}] -> Method[{}] is: {} millis", joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName(), (end - start));
         return result;
     }
 }
